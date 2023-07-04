00b054a5a5b07ca98826c33f88dbecc5e496886b
A1rcher <zhangju2879@qq.com>
2023-07-04 19:24:28 +0800

    #

---
+++ OpenCV/task_2/main.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+import cv2
+import numpy as np
+
+threshold = 100     # 声明变量
+
+img = cv2.imread('OpenCV/task_2/image/1.jpg')
+height, width, _ = img.shape
+for row in range(height):
+    for col in range(width):
+        average = sum(img[row, col])//3
+        # 二值处理
+        if average > threshold:
+            average = 255
+        else:
+            average = 0
+        
+        img[row, col] = [average, average, average]
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+        
